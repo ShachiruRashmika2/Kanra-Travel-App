9142e4f32de6c69a71c829f85cc1e56ab188ae74
ShachiruRashmika2 <it22602046@my.sliit.lk>
2025-04-30 17:13:51 +0530

    change cardGrid Location

--- frontend/src/components/MainGrid_4.jsx
+++ frontend/src/components/MainGrid_4.jsx
@@ -1,7 +1,24 @@
 import { Grid, Typography ,Box,Button, IconButton} from "@mui/material"
 import KeyboardArrowRightRoundedIcon from '@mui/icons-material/KeyboardArrowRightRounded';
 import KeyboardArrowLeftRoundedIcon from '@mui/icons-material/KeyboardArrowLeftRounded';
-import CardGrid from "./cardGrid";
+import { Card, CardContent, CardMedia, Chip } from "@mui/material";
+import LocationPinIcon from '@mui/icons-material/LocationPin';
+import Vanice from '../assets/images/pexels-pixabay-417344.jpg';
+import Iceland from '../assets/images/pexels-rudy-kirchner-278171-831056.jpg';
+import Moab from '../assets/images/pexels-brianna-eisman-547922903-23629489.jpg';
+import Arizona from '../assets/images/pexels-austin-sullivan-48171954-13594168.jpg';
+
+
+const places = [
+    { title: "Venice", image: Vanice },
+    { title: "Iceland", image: Iceland },
+    { title: "Moab", image: Moab },
+    { title: "Arizona", image: Arizona }
+   
+
+];
+
+
 const MainGrid4 = () => {
     return (
         <Grid container spacing={0} sx={{ m: '5%' ,px:0,py:1}} >
@@ -20,7 +37,59 @@ const MainGrid4 = () => {
                 </Box>
         </Grid>
         <Grid size={12} sx={{ px: 5, py:0,display:'flex'}}>
-       <CardGrid/>
+        <Box sx={{ flexGrow: 1, padding: 4, overflow: 'hidden' }}>
+            <Grid
+                container
+                spacing={4}
+                sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "nowrap",
+                    overflow: "hidden",
+                    width: 'max-content',
+                    padding: 2,
+                    alignItems: "flex-end",
+                }}
+            >
+                {places.map((place, index) => (
+                    <Grid item key={place.title}>
+                        <Card
+                            sx={{
+                                borderRadius: 4,
+                                position: "relative",
+                                height: index % 2 === 0 ? 300 : 250,
+                                width: 300
+                            }}
+                        >
+                            <CardMedia
+                                component="img"
+                                height={index % 2 === 0 ? "300" : "250"}
+                                width={300}
+                                image={place.image}
+                                alt={place.title}
+                            />
+                            <Chip
+                                label={place.title}
+                                color="primary"
+                                icon={<LocationPinIcon />}
+                                size="small"
+                                sx={{
+                                    position: "absolute",
+                                    top: 16,
+                                    [index % 2 === 0 ? "left" : "right"]: 16,
+                                    backgroundColor: "white",
+                                    color: "text.primary",
+                                    fontWeight: "bold",
+                                    '& .MuiChip-icon': {
+                                        color: 'primary.main',
+                                    }
+                                }}
+                            />
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
         </Grid>
         <Box sx={{ width: '100%', my: 2 }}>
             <hr />
